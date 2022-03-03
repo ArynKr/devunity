921895522695d4ceb1b4e527f7e4318f31ec662a
Aryan Kumar <aryankumar1504@gmail.com>
2022-03-03 16:34:34 +0530

    Added: Global Styles; Navbar complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-import StyledHeader from './styles/Header.styled';
+import { Button } from './styles/Button.styled';
+import Container from './styles/Container.styled';
+import StyledHeader, { Logo, Nav } from './styles/Header.styled';
 
 function Header() {
 	return (
 		<StyledHeader>
-			<h1>Hubble</h1>
+			<Container>
+				<Nav>
+					<Logo src='images/logo.svg' alt='logo' />
+					<Button>Try It Free</Button>
+				</Nav>
+			</Container>
 		</StyledHeader>
 	);
 }
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,8 +1,20 @@
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
-	background: #edfbff;
+	background: ${({ theme }) => theme.colors.header};
 	padding: 40px 0;
 `;
 
+const Nav = styled.nav`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 40px;
+`;
+
+const Logo = styled.img`
+	/* width: 100px; */
+`;
+
 export default StyledHeader;
+export { Nav, Logo };
